bd1283335a21434d22356a7492987642cb415d0c
sofiadinizms <sdms@cin.ufpe.br>
2023-09-17 22:33:27 -0300

    clean up

--- GuessTheFlag/ViewController.swift
+++ GuessTheFlag/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
     @IBOutlet var Button1: UIButton!
     @IBOutlet var Button2: UIButton!
     @IBOutlet var Button3: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
